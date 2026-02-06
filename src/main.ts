116903d166b8dc444bafdd5fa8741c2549901217
Igor Todorovic <igor333m@yahoo.com>
2026-02-05 22:32:06 -0800

    Add user controller, service, entity and dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /**
+       * whitelist: true / Strip (remove) any properties that are not defined on the DTOs.
+       * This prevents unexpected or malicious extra fields from reaching
+       * your handlers by only allowing validated (whitelisted) properties.
+       * Improves security and keeps inputs predictable.
+       */
+      whitelist: true
+    })
+  )
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
